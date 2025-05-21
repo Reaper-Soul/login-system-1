6d02c25b66918038804364d5303ab6c5f24d40a3
Reaper-Soul <navishsharma2004@gmail.com>
2025-05-21 01:22:22 +0000

    validate page is done with extra changes

---
+++ validate.php
@@ -0,0 +1,21 @@
+<?php
+
+session_start();
+
+$valid_username = "mike";
+$valid_password = "password";
+
+$username = $_REQUEST['username'];
+$password = $_REQUEST['password'];
+
+if ($valid_username == $username && $valid_password == $password) {
+    header('location: /');
+} else {
+    if (!isset($_SESSION['failed_attempts'])) {
+        $_SESSION['failed_attempts'] = 1;
+    } else {
+        $_SESSION['failed_attempts'] = $_SESSION['failed_attempts'] + 1;
+    }
+
+    echo "This is unsuccessful attempt number " . $_SESSION['failed_attempts'];
+}
