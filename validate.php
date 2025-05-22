ad13dff22a6e79380c5adf24bbd388fa9f530d8b
Reaper-Soul <navishsharma2004@gmail.com>
2025-05-22 23:52:07 +0000

    made the username case insensitive

--- validate.php
+++ validate.php
@@ -1,30 +1,37 @@
 <?php
 session_start();
 
-$valid_username = "Navish";
-$valid_password = "password";
+// List of valid users and their passwords
+$valid_users = [
+    "mike" => "password",
+    "jane" => "1234",
+    "navish" => "secret"
+];
 
-$username = $_POST['username'];
-$password = $_POST['password'];
+
+$username_input = strtolower($_POST['username']); 
+$password_input = $_POST['password'];
 
 
-$_SESSION['username'] = $username;
+$valid_users_lower = array_change_key_case($valid_users, CASE_LOWER);
 
-if ($valid_username === $username && $valid_password === $password) {
+
+$_SESSION['username'] = $_POST['username'];
+
+if (isset($valid_users_lower[$username_input]) && $valid_users_lower[$username_input] === $password_input) {
     $_SESSION['authenticated'] = 1;
-    $_SESSION['failed_attempts'] = 0; //reset
+    $_SESSION['failed_attempts'] = 0;
     header("Location: index.php");
     exit();
 } else {
-
+    
     if (!isset($_SESSION['failed_attempts'])) {
         $_SESSION['failed_attempts'] = 1;
     } else {
         $_SESSION['failed_attempts'] += 1;
     }
 
-    
-    echo "<p>This is unsuccessful attempt number " . $_SESSION['failed_attempts'] . "</p>";
+    echo "<p>Invalid login. This is attempt #" . $_SESSION['failed_attempts'] . "</p>";
     echo "<p><a href='login.php'>Try Again</a></p>";
 }
 ?>
